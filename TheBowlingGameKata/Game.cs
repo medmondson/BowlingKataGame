ae185a524a2c1d662e605f2cbbb4bf52d278fd8b
eddy556 <eddy556@gmail.com>
2014-03-05 23:32:13 +0000

    Initial Commit

---
+++ TheBowlingGameKata/Game.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBowlingGameKata
+{
+    public class Game
+    {
+        private int score;
+
+        public void roll(int pins)
+        {
+            score += pins;
+        }
+
+        public int Score()
+        {
+            return score;
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheBowlingGameKata;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class BowlingGameTest
+    {
+        private Game g;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            g = new Game();
+        }
+
+        [TestMethod]
+        public void TestGutterGame()
+        {
+            for (int i = 0; i<20; i++)
+            {
+                g.roll(0);
+            }
+
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void TestAllOnes()
+        {
+            for (int i = 0; i < 20; i++)
+                g.roll(1);
+
+            Assert.AreEqual(20, g.Score());
+        }
+    }
+}
